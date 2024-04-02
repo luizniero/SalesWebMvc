1346fc528d96b0d5cd7988f7d5b30002a7a5c698
Luiz Henrique Pereira Niero <luiz_niero@hotmail.com>
2024-04-01 22:42:16 -0300

    Criando e adicionando ao startup uma classe que popula o banco com valores de exemplos

---
+++ Data/SeedingService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            // DB já existe (já tem registros)
+            if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any()) return;
+
+
+            Department d1 = new Department()
+            {
+                Id = 1,
+                Name = "Computers",
+            };
+            Department d2 = new Department()
+            {
+                Id = 2,
+                Name = "Eletronics",
+            };
+            Department d3 = new Department()
+            {
+                Id = 3,
+                Name = "Fashon",
+            };
+            Department d4 = new Department()
+            {
+                Id = 4,
+                Name = "Books",
+            };
+
+            Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new System.DateTime(1992, 02, 01), 1000.0, d1);
+            Seller s2 = new Seller(1, "Sasha Grey", "sasha@gmail.com", new System.DateTime(1992, 02, 01), 1000.0, d1);
+            Seller s3 = new Seller(1, "Tony Red", "tony@gmail.com", new System.DateTime(1992, 02, 01), 1000.0, d1);
+            Seller s4 = new Seller(1, "Carmen blue", "carmen@gmail.com", new System.DateTime(1992, 02, 01), 1000.0, d1);
+
+            SalesRecord r1 = new SalesRecord(1, new System.DateTime(2018, 09, 25), 1.000, SaleStatus.Billed, s1);
+            SalesRecord r2 = new SalesRecord(2, new System.DateTime(2019, 09, 25), 2.000, SaleStatus.Billed, s2);
+            SalesRecord r3 = new SalesRecord(3, new System.DateTime(2020, 09, 25), 3.000, SaleStatus.Billed, s3);
+            SalesRecord r4 = new SalesRecord(4, new System.DateTime(2021, 09, 25), 4.000, SaleStatus.Billed, s4);
+
+            _context.Department.AddRange(d1, d2, d3, d4);
+            _context.Seller.AddRange(s1, s2, s3, s4);
+            _context.SalesRecord.AddRange(r1, r2, r3, r4);
+
+            _context.SaveChanges();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,14 +29,19 @@ namespace SalesWebMvc
 
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+
+            // Adicionando a classe SeedingService como se fosse um serviço na injeção de dependência.
+            // Permite que eu adicione ela no 'configure'.
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed(); // Popula o banco de dados com valores iniciais de teste.
             }
             else
             {
