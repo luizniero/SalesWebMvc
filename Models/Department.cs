548965befc2ddc9fa6be76d6dc860bc73edb4d10
Luiz Henrique Pereira Niero <luiz_niero@hotmail.com>
2024-04-01 22:16:13 -0300

    Criando os models da aplicação (Aula 260)

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,35 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Seller> Sellers { get; set;} = new List<Seller>();
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Department()
+        {
+        }
+
+        private void addSeller(Seller seller) 
+        {
+            this.Sellers.Add(seller);
+        }
+
+        private double totalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(initial, final));
+        }
     }
 }
